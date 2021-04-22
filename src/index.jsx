addd2d927bcd1b44ac37988bbd1226f2fbcc0e31
Alexander Botwinick <alexbotwinick@gmail.com>
2021-04-22 17:35:53 +0200

    app component and index.jsx with root

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,20 @@
 // external modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
 // internal modules
-import App from './components/app';
-import '../assets/stylesheets/application.scss';
+import App from "./components/app";
+import "../assets/stylesheets/application.scss";
 
 // State and reducers
+import CitiesReducer from "./reducers/cities_reducer";
+import ActiveCityReducer from "./reducers/active_city_reducer";
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  cities: CitiesReducer,
+  activeCity: ActiveCityReducer,
 });
 
 // render an instance of the component in the DOM
@@ -18,5 +22,5 @@ ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
